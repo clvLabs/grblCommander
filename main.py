6f6bcb6d51e79cc72ec5a8289b26fbda1c7b5912
Tony Aguilar <onixag@gmail.com>
2017-08-06 09:59:04 +0200

    Version bump: 0.4.1

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ mchCfg = cfg['machine']
 mcrCfg = cfg['macro']
 
 # Current version
-gVERSION = '0.4.0'
+gVERSION = '0.4.1'
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 def readyMsg(extraInfo=None):
