0e9b868fb7eeeff691f0d2e4e12d0733767d4ce9
Tony Aguilar <onixag@gmail.com>
2017-08-08 01:42:04 +0200

    Bug fix

--- src/config.py
+++ src/config.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
   print('This file is a module, it should not be executed directly')
 
 try:
-  from src.config_default import cfg as default
-  cfg = default
-except:
   from src.config_user import cfg as user
   cfg = user
+except:
+  from src.config_default import cfg as default
+  cfg = default
